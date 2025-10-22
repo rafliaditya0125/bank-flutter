eb8e1b8c21f75e92300a2bf88a6df1d626ab0fc3
RafliRV <ra20081112@gmail.com>
2025-10-22 19:05:32 +0700

    add home page

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,13 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => LoginService())],
+      providers: [
+        ChangeNotifierProvider(create: (_) => LoginService()),
+        ChangeNotifierProvider(create: (_) => BankService()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/',
+        initialRoute: '/home',
         routes: {
           '/': (context) => SplashPage(),
           '/login': (context) => LoginPage(),
--- lib/properties.dart
+++ lib/properties.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 
 class Properties {
   static const Color mainColor = Color(0xFF8700C3);
@@ -98,5 +101,58 @@ class LoginService extends ChangeNotifier {
       setLoginErrorMessage('Error during sign in: ' + ex.message!);
       return false;
     }
+  }
+}
+
+class Account {
+  String? id;
+  String? type;
+  String? accountNumber;
+  double? balance;
+
+  Account({this.id, this.type, this.accountNumber, this.balance});
+
+  factory Account.fromJson(Map<String, dynamic> json, String docId) {
+    return Account(
+      id: docId,
+      type: json['type'],
+      accountNumber: json['account_number'],
+      balance: json['balance'],
+    );
+  }
+}
+
+class BankService extends ChangeNotifier {
+  Future<List<Account>> getAccounts(BuildContext context) {
+    LoginService loginService = Provider.of<LoginService>(
+      context,
+      listen: false,
+    );
+    String userId = loginService.getUserId();
+
+    List<Account> accounts = [];
+
+    Completer<List<Account>> accountsCompleter = Completer();
+
+    FirebaseFirestore.instance
+        .collection('accounts')
+        .doc('9vgwHArNTUoRnlkZRIaq')
+        .collection('user_accounts')
+        .get()
+        .then((QuerySnapshot collection) {
+          for (var doc in collection.docs) {
+            var acctDoc = doc.data() as Map<String, dynamic>;
+            var acct = Account.fromJson(acctDoc, doc.id);
+            accounts.add(acct);
+          }
+
+          Future.delayed(Duration(seconds: 1), () {
+            accountsCompleter.complete(accounts);
+          });
+        })
+        .catchError((error) {
+          accountsCompleter.complete([]);
+        });
+    return accountsCompleter.future;
   }
 }
